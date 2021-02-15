8a84f8cc9cc4cad4ac96b13e7f5c2ff737939493
Mathew E Newton <elijah2895@hotmail.com>
2021-02-15 23:00:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,8 +17,8 @@ def load_library(path)
 end
 
 def get_japanese_emoticon(path, emoticon)
-  final_emoticons = load_library(path)
-  final_emoticons.each do |key, description|
+  final_emojis = load_library(path)
+  final_emojis.each do |key, description|
     if description[:japanese] == emoticon
       return key
     end
